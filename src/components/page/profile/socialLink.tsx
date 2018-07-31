5e54699d1185e2cb45b3da81e0bf756381066581
john <sijunyan666@gmail.com>
2018-07-31 23:26:28 +1200

    update links

--- src/components/page/profile/socialLink.tsx
+++ src/components/page/profile/socialLink.tsx
@@ -7,15 +7,15 @@ export class SocialLink extends React.Component {
         <div className='social-link'>
             <div>
                 <i className="fab fa-facebook" />
-                <a href='https://www.facebook.com/si.ji.16'>https://www.facebook.com</a>
+                <a href='https://www.facebook.com/si.ji.16'>https://www.facebook.com/si.ji.16</a>
             </div>
             <div>
                 <i className="fab fa-linkedin" />
-                <a href='https://www.linkedin.com/in/john-yan-2b839189/'>https://www.linkedin.com</a>
+                <a href='https://www.linkedin.com/in/john-yan-2b839189/'>https://www.linkedin.com/in/john-yan-2b839189/</a>
             </div>
             <div>
                 <i className="fab fa-stack-overflow" />
-                <a href='https://stackoverflow.com/users/3188910/justwe?tab=profile'>https://stackoverflow.com/</a>
+                <a href='https://stackoverflow.com/users/3188910/justwe?tab=profile'>https://stackoverflow.com/users/justwe</a>
             </div>
         </div>
       );
