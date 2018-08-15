cda9ab25b9bf00985b60fc2904e1dcba126858bd
john <sijunyan666@gmail.com>
2018-08-15 22:06:41 +1200

    add drop caps to about me

--- src/components/page/profile/aboutMe.tsx
+++ src/components/page/profile/aboutMe.tsx
@@ -4,7 +4,7 @@ export class AboutMe extends React.Component {
 
   public render() {
     return (
-      <div>
+      <div className='first-letter'>
         Degree-qualified software engineer with more than 7 years
 commercial experience across the SDLC and Microsoft technology
 stack. Commercial leadership, training and software development
