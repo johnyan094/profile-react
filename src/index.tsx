b2861be65484b44b4a4b251d224fa28485dcec79
johnyan <john.yan@cin7.com>
2019-05-20 16:12:42 +1200

    add express

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,20 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+const express = require('express');
+const path = require('path');
+const app = express();
+
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
