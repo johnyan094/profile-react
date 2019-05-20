c4545c691404c1c9545ba83ee77de416caa9b35d
johnyan <john.yan@cin7.com>
2019-05-20 18:07:23 +1200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-const express = require('express');
-const path = require('path');
-const app = express();
 
 
 ReactDOM.render(
@@ -14,9 +11,3 @@ ReactDOM.render(
 );
 registerServiceWorker();
 
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
